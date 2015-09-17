7b9314af38910de166b923a029d2a24df1f70e99
Niek <2andrese@informatik.uni-hamburg.de>
2015-09-17 19:36:38 +0200

    wrote some todo about a flag in URFdataStruct

--- URFcode/URFdataStruct.h
+++ URFcode/URFdataStruct.h
@@ -5,6 +5,7 @@
 
 /** struct that contains all data that was accumulated during the calculation of the URF-structure.
 The URF.h user does not need to change or read anything in this structure other than by using the functions provided in "URF.h". */
+/*TODO something to write: This could contain a flag storing if the URFs have been calculated before for this graph. Every function called would first check if this flag is set and call 'calculateURFs()' if it is not. This would make the 'calculateURFs()' call unnecessary. I won't do this because it would require the user to initialize his urfdata ** to NULL before calling the function for the first time (or get "used uninitialized.."). Also he would have to have a urfdata ** instead of a urfdata * which requires dereferencing one more time.*/
 typedef struct{
     GraphURF *graph;
     unsigned int nofURFs;//number of URFs
