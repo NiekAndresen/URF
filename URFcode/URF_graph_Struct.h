9d37de86dae729db5834d15fee60db28acc93c9e
Niek <2andrese@informatik.uni-hamburg.de>
2015-10-21 22:33:09 +0200

    renamed the files for the graph struct

---
+++ URFcode/URF_graph_Struct.h
@@ -0,0 +1,14 @@
+/* Niek Andresen 2015 - Bachelor Thesis */
+
+#ifndef URFGRAPHSTRUCT
+#define URFGRAPHSTRUCT
+
+typedef struct{
+    int V, E; /*number of vertices/edges*/
+    int *degree; /*array storing how many vertices are adjacent to each vertex*/
+    int **adjList; /*the vertices are stored with their index (0 to |V|-1)*/
+    int **edges; /*array containing pairs of vertices as edges*/
+    int edgesAlloced; /*space alloced for this many edges in 'edges' array*/
+} URF_graph;
+
+#endif
